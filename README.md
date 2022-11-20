465af987213c613d9315558b9c242c7dd0520b5b
Kenneth Watson <git@shrimpworks.za.net>
2022-11-20 20:03:16 +0800

    Add license, readme, adjust startup

---
+++ README.md
@@ -0,0 +1,28 @@
+# Proggers
+
+Proggers is progress-bars-as-a-service!
+
+Quickly create hosted progress bars for tracking long-running (or any other!)
+processes using a dead-simple HTTP API.
+
+See and use it at [https://proggers.cloud/](https://proggers.cloud/)
+
+## Building
+
+```
+# simple build
+./gradlew build
+
+# build a distributable java package
+./gradlew assembleDist
+```
+
+## Running
+
+It's not configurable right now, so you may want to twiddle some values in 
+`Main.java` for data store and port.
+
+After executing an `assembleDist`, unzip/untar the output in 
+`build/distributions`, change to the `proggers` directory that was extracted,
+and execute `./bin/proggers` (make this executable if you used the `.zip`
+package).
--- src/main/java/net/shrimpworks/proggers/Main.java
+++ src/main/java/net/shrimpworks/proggers/Main.java
@@ -2,6 +2,7 @@ package net.shrimpworks.proggers;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import net.shrimpworks.proggers.service.ProgressService;
@@ -10,11 +11,19 @@ import net.shrimpworks.proggers.www.WebService;
 
 public class Main {
 
+	private static final String DEFAULT_BIND_HOST = "127.0.0.1";
+	private static final int DEFAULT_PORT = 8088;
+	private static final String DATA_PATH = "data"; // assumes to be in PWD
+
 	public static void main(String[] args) throws IOException {
-		final ProgressStore store = new ProgressStore.FileStore(Paths.get("data"));
+		Path dataPath = Paths.get(DATA_PATH).toAbsolutePath();
+
+		final ProgressStore store = new ProgressStore.FileStore(dataPath);
 		final ProgressService service = new ProgressService(store);
 
-		final WebService webService = new WebService(service, new InetSocketAddress("127.0.0.1", 8088));
+		final WebService webService = new WebService(service, new InetSocketAddress(DEFAULT_BIND_HOST, DEFAULT_PORT));
+
+		System.out.printf("Started and listening on port %d with data store in %s%n", DEFAULT_PORT, dataPath);
 
 		Runtime.getRuntime().addShutdownHook(new Thread(webService::close));
 	}
